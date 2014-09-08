209e4460b629a5a6ca7b1f12083465031b4594f8
co8bit <co8bit@gmail.com>
2014-09-08 17:49:04 +0800

    重新新建整个工程，创建全新的数据库表

--- Conf/MyConfigINI.php
+++ Conf/MyConfigINI.php
@@ -1,27 +1,13 @@
 <?php
-define("_SOFTNAME","爱情银行");//如若更改，则Tpl/Public/title.html文件也需要更改
-define("_VERSION","1.0.0");
-define("_CURRENCY","爱情币");
-define("_INIT_MONEY",100);
-define("_SPECIAL_END_FLAG","@#$%^&*&^%$#@!$(&&$%^");
-define("_SPECIAL_END_FLAG_STRLEN",strlen(_SPECIAL_END_FLAG));
-define("_SELECT_CONTENT_BREAK_FLAG","--------做到+");
-define("_SELECT_CONTENT_BREAK_FLAG_STRLEN",strlen(_SELECT_CONTENT_BREAK_FLAG));
-define("_SPECAL_BILL_END_FLAG","%$#@");
-define("_SPECAL_BILL_END_FLAG_STRLEN",strlen(_SPECAL_BILL_END_FLAG));
-define("_SPECAL_DIARY_END_FLAG",_SPECAL_BILL_END_FLAG);
-define("_SPECAL_DIARY_END_FLAG_STRLEN",strlen(_SPECAL_DIARY_END_FLAG));
-define("_SELECT_NOTE_BREAK_FLAG",_SPECAL_BILL_END_FLAG);
-define("_SELECT_NOTE_BREAK_FLAG_STRLEN",strlen(_SELECT_NOTE_BREAK_FLAG));
-define("_ADD_REMARK","有你陪伴很开心");
-define("_SUB_REMARK","你没做到约定");
-define("_DEBUG",false);
+define("_SPECAL_BREAK_FLAG","@#$%^&*");
+define("_ORIGIN_PREFIX","origin_");//原始数组的名称前缀。commonModel中ModelBaseOP用
+define("_AJAX_BREAK_TAG","~;^");//AJAX输出时候的控制终端的标志
 
-$_ANNOUNCEMENT = array("本系统正在测试","已升级成为1.0.0版本","空","空","空","空","空");//预留7个空
-/*
- 权限：
-销售     进货     库存管理     应收款     应付款     员工管理   财务  账号 
-0     1    2     3    4      5    6  7
-*/
-$_POWERCHINESE = array("销售","进货","库存","应收款","应付款","员工管理","财务","账号");
+//+=====================安全========================================+
+define("_ISHTTPS",false);//是否开启强制https连接。注：Public、Print两个Action没有这个
+define("_KEY128","ayuiowlm78^%ew%.");//ASE加密算法中的key。16B=128bit
+define("_IV","789asUI{}:asdag1");//ASE加密算法中的初始向量。16B=128bit
+define("BLOCKSIZE",33);//标签组里最多能显示多少个方格（用于首页）
+
+define("COMPANYNAME","快点订餐系统");//软件注册的公司名称，用于excel输出
 ?>
